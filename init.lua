379b7640dddc132008c418cc7979cebcaa785527
miyagawadaiki <miyagawa.daiki.18@shizuoka.ac.jp>
2025-05-11 18:14:23 +0900

    ズーム機能を追加
    モード切り替えにも気を使ってある（はず）

--- init.lua
+++ init.lua
@@ -4,8 +4,48 @@ local lastMousePosition = nil
 local inactivityTimer = nil
 local scrollSpeed = 10  -- スクロールの感度（大きくすると速くなる）
 
+-- 拡大縮小モード制御フラグ
+local isZoomMode = false
+local zoomExecResolution = 20.0  -- ズームを実行する分解能
+local totalMovementX = 0
+local zoomExecThreshold = 0
+
+
 local alertId = nil
 
+function controlMode(mode)
+	if mode == "Scroll" then
+		if isScrollMode then
+			exitScrollMode()
+			return
+		end
+
+		if isZoomMode then
+			exitZoomMode()
+		--elseif isUndoMode then
+		--	exitUndoMode()
+		end
+
+		enterScrollMode()
+	
+	elseif mode == "Zoom" then
+		if isZoomMode then
+			exitZoomMode()
+			return
+		end
+
+		if isScrollMode then
+			exitScrollMode()
+		--elseif isUndoMode then
+		--	exitUndoMode()
+		end
+
+		enterZoomMode()
+	end
+end
+
+
+-- スクロールモードをスイッチする
 function toggleScrollMode()
     if isScrollMode then
         exitScrollMode()
@@ -14,6 +54,7 @@ function toggleScrollMode()
     end
 end
 
+-- スクロールモードに入る
 function enterScrollMode()
     isScrollMode = true
     lastMousePosition = hs.mouse.absolutePosition()
@@ -21,6 +62,7 @@ function enterScrollMode()
     startInactivityTimer()
 end
 
+-- スクロールモードから出る
 function exitScrollMode()
     isScrollMode = false
     showTempAlert("Scroll Mode OFF", 0.4)
@@ -29,6 +71,40 @@ function exitScrollMode()
         inactivityTimer = nil
     end
 end
+
+
+-- ズームモードをスイッチする
+function toggleZoomMode()
+    if isZoomMode then
+        exitZoomMode()
+    else
+        enterZoomMode()
+    end
+end
+
+-- ズームモードに入る
+function enterZoomMode()
+    isZoomMode = true
+    lastMousePosition = hs.mouse.absolutePosition()
+	totalMovementX = 0.0
+	zoomExecThreshold = 0.0
+    showTempAlert("Zoom Mode ON", 0.4)
+    startInactivityTimer()
+end
+
+-- ズームモードから出る
+function exitZoomMode()
+    isZoomMode = false
+    showTempAlert("Zoom Mode OFF", 0.4)
+    if inactivityTimer then
+        inactivityTimer:stop()
+        inactivityTimer = nil
+    end
+end
+
+
+
+
 
 function showTempAlert(message, duration)
     if alertId then
@@ -42,25 +118,48 @@ function startInactivityTimer()
     inactivityTimer = hs.timer.doAfter(1, function()
         if isScrollMode then
             exitScrollMode()
+		elseif isZoomMode then
+			exitZoomMode()
         end
     end)
 end
 
 -- マウス移動を監視
 mouseTracker = hs.eventtap.new({hs.eventtap.event.types.mouseMoved}, function(event)
-    if not isScrollMode then return false end
+    if not isScrollMode and not isZoomMode then return false end
 
     local currentPosition = hs.mouse.absolutePosition()
     if lastMousePosition then
-        local dx = event:getProperty(hs.eventtap.event.properties['mouseEventDeltaX'])
-        --local dx = currentPosition.x - lastMousePosition.x
-        local dy = event:getProperty(hs.eventtap.event.properties['mouseEventDeltaY'])
-        --local dy = currentPosition.y - lastMousePosition.y
+		local dx = event:getProperty(hs.eventtap.event.properties['mouseEventDeltaX'])
+		--local dx = currentPosition.x - lastMousePosition.x
+		local dy = event:getProperty(hs.eventtap.event.properties['mouseEventDeltaY'])
+		--local dy = currentPosition.y - lastMousePosition.y
 
-        --print(string.format("Mouse moved: dx = %.2f, dy = %.2f", dx, dy))
+		--print(string.format("Mouse moved: dx = %.2f, dy = %.2f", dx, dy))
+		
+		-- スクロール
+		if isScrollMode then
 
-        -- スクロールイベント送信（dyは方向反転）
-        hs.eventtap.event.newScrollEvent({ -dx * scrollSpeed, -dy * scrollSpeed }, {}, "pixel"):post()
+			-- スクロールイベント送信（dyは方向反転）
+			hs.eventtap.event.newScrollEvent({ -dx * scrollSpeed, -dy * scrollSpeed }, {}, "pixel"):post()
+
+		-- ズーム
+		else
+			totalMovementX = totalMovementX + dx
+			print(string.format("Mouse moved: dx = %.2f, total = %.2f", dx, totalMovementX))
+			-- 拡大 or 縮小
+			if dx > 0 and totalMovementX > zoomExecThreshold + zoomExecResolution then
+				-- ズームインを実行
+				hs.eventtap.keyStroke({"cmd"}, ";")
+				-- 閾値を更新
+				zoomExecThreshold = zoomExecThreshold + zoomExecResolution
+			elseif dx < 0 and totalMovementX < zoomExecThreshold - zoomExecResolution then
+				-- ズームアウトを実行
+				hs.eventtap.keyStroke({"cmd"}, "-")
+				-- 閾値を更新
+				zoomExecThreshold = zoomExecThreshold - zoomExecResolution
+			end
+		end
 
 		-- カーソルは元の位置に戻す
 		hs.mouse.absolutePosition(lastMousePosition)
@@ -74,8 +173,14 @@ end)
 
 mouseTracker:start()
 
--- F1キーをホットキーに割り当て
+-- F1キーをスクロールのホットキーに割り当て
 hs.hotkey.bind({}, "F1", function()
-    toggleScrollMode()
+	controlMode("Scroll")
+    --toggleScrollMode()
 end)
 
+-- F2キーをズームのホットキーに割り当て
+hs.hotkey.bind({}, "F2", function()
+	controlMode("Zoom")
+    --toggleScrollMode()
+end)
